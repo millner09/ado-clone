73f04bae7bc9a1735a2e115596bd69752952a6a4
Adam Millner <adam.millner@gmail.com>
2022-08-25 21:14:18 -0500

    Scaffolded out projects.

---
+++ Infrastructure/Security/UserAccessor.cs
@@ -0,0 +1,26 @@
+﻿using Application.Interfaces;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Security
+{
+    public class UserAccessor : IUserAccessor
+    {
+        private readonly IHttpContextAccessor httpContextAccesor;
+
+        public UserAccessor(IHttpContextAccessor httpContextAccesor)
+        {
+            this.httpContextAccesor = httpContextAccesor;
+        }
+
+        public string GetUserName()
+        {
+            return httpContextAccesor.HttpContext.User.FindFirstValue(ClaimTypes.Name);
+        }
+    }
+}
