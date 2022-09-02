5e4ff86458ebd5cb503c085ee6465a0473761c99
Adam Millner <adam.millner@gmail.com>
2022-09-01 19:42:05 -0500

    Setup end to end for fetching weather
    Created Weather Component
    
    Created Weather Service
    
    Create Weather Interface

--- Api/Extensions/ApplicationServiceExtensions.cs
+++ Api/Extensions/ApplicationServiceExtensions.cs
@@ -30,7 +30,7 @@ namespace Api.Extensions
             {
                 opt.AddPolicy("CorsPolicy", policy =>
                 {
-                    policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
+                    policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:4200");
                 });
             });
 
