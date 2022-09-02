370650ef99cfed3bc7e295b5789e9b7e17b87f52
Adam Millner <adam.millner@gmail.com>
2022-09-01 19:59:14 -0500

    Added angular material

--- client-app/src/app/app.module.ts
+++ client-app/src/app/app.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 
+import {MatCardModule} from '@angular/material/card'; 
+
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { WeatherComponent } from './weather/weather.component';
@@ -14,7 +16,8 @@ import { WeatherComponent } from './weather/weather.component';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    HttpClientModule
+    HttpClientModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- client-app/src/app/weather/weather.component.ts
+++ client-app/src/app/weather/weather.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { Weather } from '../weather';
 import { WeatherService } from '../weather.service';
 
-
 @Component({
   selector: 'app-weather',
   templateUrl: './weather.component.html',
