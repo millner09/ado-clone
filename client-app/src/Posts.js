3e6d629578537f2ed901b40c218379891c430a61
Adam Millner <adam.millner@gmail.com>
2022-09-07 14:18:13 -0500

    Updated client-app
    Removed Angular.
    
    Added React.
    
    Added @auth0-auth0-react to client-app.
    
    Successfully authenticating with auth0 and fetching data from webapi.

---
+++ client-app/src/Posts.js
@@ -0,0 +1,49 @@
+import React, { useEffect, useState } from "react";
+import { useAuth0 } from "@auth0/auth0-react";
+
+const Posts = () => {
+  const { getAccessTokenSilently } = useAuth0();
+  const [weathers, setWeathers] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const token = await getAccessTokenSilently({
+          audience: "https://amillner-ado-clone/",
+        });
+        const response = await fetch(
+          "http://localhost:5167/api/weatherforecast",
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        );
+
+        var body = await response.json();
+        setWeathers(body);
+        console.log(body);
+      } catch (e) {
+        console.error(e);
+      }
+    })();
+  }, [getAccessTokenSilently]);
+
+  if (!weathers) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <ul>
+      {weathers.map((weather, index) => {
+        return (
+          <li key={index}>
+            {weather.date} - {weather.temperatureF}
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
+
+export default Posts;
