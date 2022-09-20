f53cd1aef7cdb30d7fa7ead39f96b782ca78291a
Adam Millner <adam.millner@gmail.com>
2022-09-20 07:26:28 -0500

    Create WorkItemType functionality added.

---
+++ Api/Controllers/WorkItemTypeController.cs
@@ -0,0 +1,31 @@
+﻿using Application.WorkItemTypes;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class WorkItemTypeController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public WorkItemTypeController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> CreateWorkItemType(CreateWorkItemType.Command command)
+        {
+            var res = await _mediator.Send(command);
+            return Ok(res);
+        }
+    }
+}
