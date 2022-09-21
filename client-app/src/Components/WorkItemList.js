e403bce3f8a9e61b0747dc339e3921489c65150f
Adam Millner <adam.millner@gmail.com>
2022-09-21 16:44:56 -0500

    Added fetching & rendering of WorkItemTypes.

--- client-app/src/Components/WorkItemList.js
+++ client-app/src/Components/WorkItemList.js
@@ -1,13 +1,15 @@
 import { useEffect, useState } from "react"
 import { useAuth0 } from "@auth0/auth0-react";
 import WorkItem from "./WorkItem";
+import WorkItemType from "./WorkItemType";
 
 const WorkItemList = () => {
     const { getAccessTokenSilently } = useAuth0();
     const [workItems, setWorkItems] = useState([]);
+    const [workItemTypes, setWorkItemTypes] = useState([]);
 
     useEffect(() => {
-        const fetchData = async () => {
+        const fetchWorkItems = async () => {
             const token = await getAccessTokenSilently({
                 audience: "https://amillner-ado-clone/",
             });
@@ -24,8 +26,25 @@ const WorkItemList = () => {
             setWorkItems(body);
         }
 
+        const fetchWorkItemTypes = async () => {
+            const token = await getAccessTokenSilently({
+                audience: "https://amillner-ado-clone/",
+            });
 
-        fetchData();
+            const res = await fetch(`${process.env.REACT_APP_API_URL}/workItemType`,
+                {
+                    headers: {
+                        Authorization: `Bearer ${token}`,
+                    },
+                }
+            );
+
+            var body = await res.json();
+            setWorkItemTypes(body);
+        }
+
+        fetchWorkItems();
+        fetchWorkItemTypes();
     }, [getAccessTokenSilently])
 
     return (
@@ -33,6 +52,11 @@ const WorkItemList = () => {
             <h1>WorkItemList</h1>
             {workItems.map((workItem) => (
                 <WorkItem key={workItem.id} workItem={workItem} />
+            ))}
+
+            <h1>WorkItemTypes</h1>
+            {workItemTypes.map((workItemType) => (
+                <WorkItemType key={workItemType.id} workItemType={workItemType} />
             ))}
         </>
     )
---
+++ client-app/src/Components/WorkItemType.js
@@ -0,0 +1,7 @@
+const WorkItemType = ({ workItemType }) => {
+    return (
+        <h3>{workItemType.name}</h3>
+    )
+}
+
+export default WorkItemType
