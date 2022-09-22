e3aa30499c30b66c8783613101f67c396ca17630
Adam Millner <adam.millner@gmail.com>
2022-09-22 10:33:24 -0500

    Reorganized react components.

--- client-app/src/index.js
+++ client-app/src/index.js
@@ -1,7 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import WorkItemList from "./components/WorkItemList"
+import NavBar from "./components/NavBar/NavBar";
+import WorkItemList from "./components/WorkItem/WorkItemList"
+import WorkItemTypeList from "./components/WorkItemType/WorkItemTypeList";
 import reportWebVitals from "./reportWebVitals";
 import { Auth0Provider } from "@auth0/auth0-react";
 import {
@@ -9,8 +11,6 @@ import {
   Route,
   Routes,
 } from "react-router-dom";
-import NavBar from "./components/NavBar";
-import WorkItemTypeList from "./components/WorkItemTypeList";
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
