bb9aab27f27df1b17b0de815483cd6d5b6a29998
Adam Millner <adam.millner@gmail.com>
2022-09-23 08:48:59 -0500

    Added protected routes.

--- client-app/src/index.js
+++ client-app/src/index.js
@@ -5,32 +5,49 @@ import NavBar from "./components/NavBar/NavBar";
 import WorkItemList from "./components/WorkItem/WorkItemList"
 import WorkItemTypeList from "./components/WorkItemType/WorkItemTypeList";
 import reportWebVitals from "./reportWebVitals";
-import { Auth0Provider } from "@auth0/auth0-react";
+import { Auth0Provider, withAuthenticationRequired } from "@auth0/auth0-react";
 import {
   BrowserRouter,
   Route,
   Routes,
+  useNavigate
 } from "react-router-dom";
 
+const ProtectedRoute = ({ component, ...args }) => {
+  const Component = withAuthenticationRequired(component, args);
+  return <Component />;
+};
+
+const Auth0ProviderWithRedirectCallback = ({ children, ...props }) => {
+  const navigate = useNavigate();
+  const onRedirectCallback = (appState) => {
+    navigate((appState && appState.returnTo) || window.location.pathname);
+  };
+  return (
+    <Auth0Provider onRedirectCallback={onRedirectCallback} {...props}>
+      {children}
+    </Auth0Provider>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <Auth0Provider
-    domain="millner.auth0.com"
-    clientId="k4pavHGKY71S2MibLXJGsQGiTJ6MFtlO"
-    redirectUri={window.location.origin}
-  >
-    <React.StrictMode>
-      <BrowserRouter>
+  <BrowserRouter>
+    <Auth0ProviderWithRedirectCallback
+      domain="millner.auth0.com"
+      clientId="k4pavHGKY71S2MibLXJGsQGiTJ6MFtlO"
+      redirectUri={window.location.origin}
+    >
+      <React.StrictMode>
         <NavBar />
         <Routes>
           <Route path="/" element={<App />} />
-          <Route path="/workItems" element={<WorkItemList />} />
-          <Route path="/workItemTypes" element={<WorkItemTypeList />} />
+          <Route path="/workItems" element={<ProtectedRoute component={WorkItemList} />} />
+          <Route path="/workItemTypes" element={<ProtectedRoute component={WorkItemTypeList} />} />
         </Routes>
-      </BrowserRouter>
-    </React.StrictMode>
-  </Auth0Provider>
+      </React.StrictMode>
+    </Auth0ProviderWithRedirectCallback>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
